a51df2df13f27d08de01124324d6bf94c022c7a4
Nauman Umer <nmanumr@gmail.com>
2024-11-25 16:53:55 +0500

    Add metadata generation function for poem pages
    This commit introduces a `generateMetadata` function to dynamically create metadata for poem pages using the content from imported JSON files. The generated metadata includes the title, description, Open Graph, and Twitter card information to enhance the page's SEO and social media presence.

--- src/app/[book]/[section]/[poem]/page.tsx
+++ src/app/[book]/[section]/[poem]/page.tsx
@@ -1,10 +1,46 @@
 import { SizeProvider, Stanza } from "./components";
-import type PoemType from '@/assets/content/armaghan-e-hijaz/01/01.json';
+import type PoemType from "@/assets/content/armaghan-e-hijaz/01/01.json";
+import { Metadata } from "next";
 
-export default async function Poem({ params }: { params: Promise<{ book: string; section: string; poem: string }> }) {
+type Props = {
+  params: Promise<{ book: string; section: string; poem: string }>;
+};
+
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
   const { book, section, poem: poemId } = await params;
 
-  const poem = await import(`../../../../assets/content/${book}/${section}/${poemId}.json`).then((m) => m.default as typeof PoemType);
+  const poem = await import(`../../../../assets/content/${book}/${section}/${poemId}.json`).then(
+    (m) => m.default as typeof PoemType,
+  );
+
+  const description = `Explore the profound words of Allama Iqbal in '${poem?.name}' from his book '${poem.bookName}'. Dive deep into his philosophical and poetic genius.`;
+  const title = `${poem?.name} - ${poem.bookName}`;
+
+  return {
+    title: `${title} | allama-iqbal.com`,
+    openGraph: {
+      type: 'article',
+      title,
+      description,
+      siteName: 'Iqbal\'s Poetry',
+    },
+    twitter: {
+      card: 'summary',
+      title,
+      description,
+    },
+    authors: {name: 'Allama Iqbal'},
+    keywords: `Iqbal, Poetry, ${poem.bookName}, ${poem?.name}, Allama Iqbal Poems, Urdu Poetry, Persian Poetry`,
+    description,
+  };
+}
+
+export default async function Poem({ params }: Props) {
+  const { book, section, poem: poemId } = await params;
+
+  const poem = await import(`../../../../assets/content/${book}/${section}/${poemId}.json`).then(
+    (m) => m.default as typeof PoemType,
+  );
 
   const urTrans = true;
   const enTrans = true;
