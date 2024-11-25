57b71caafee922e0d392d18766da9cc624489a27
Nauman Umer <nmanumr@gmail.com>
2024-11-25 09:59:05 +0500

    update

---
+++ src/app/[book]/[section]/[poem]/components.tsx
@@ -0,0 +1,127 @@
+"use client";
+
+import { createContext, PropsWithChildren, use, useEffect, useRef, useState } from "react";
+import clsx from "clsx";
+import type PoemType from "@/assets/content/armaghan-e-hijaz/01/01.json";
+
+const SizeContext = createContext<{
+  setChildSize: (size: number) => void;
+  maxSize: number | undefined;
+} | null>(null);
+
+export function Stanza({
+  para,
+  urTrans,
+  enTrans,
+}: {
+  para: (typeof PoemType)["Para"][0];
+  urTrans: boolean;
+  enTrans: boolean;
+}) {
+  const verses = Array.isArray(para.Verse) ? para.Verse : [para.Verse];
+
+  return (
+    <>
+      {para.name && <div className="my-4 text-start text-2xl">{para.name}</div>}
+
+      {verses.map((couplet) => {
+        const originalText = couplet.Text.find((node) => node.lang === "Original")
+          ?._content?.split("\n")
+          .map((v) => v?.trim())
+          .filter(Boolean);
+
+        const urduText = couplet.Text.find((node) => node.lang === "Urdu")?._content;
+        const englishText = couplet.Text.find((node) => node.lang === "English")?._content;
+
+        const id = couplet.id;
+
+        return (
+          <SizeProvider>
+            <div
+              className={clsx(
+                "relative w-full max-w-6xl border-b border-black/10 px-4 py-2",
+                (urTrans || enTrans) && "grid-cols-5 lg:grid lg:gap-x-10",
+              )}
+              id={`cplt${id}`}
+              key={id}
+            >
+              <div className="relative col-span-2 flex flex-col justify-center ps-8">
+                {id && (
+                  <a
+                    href={`#cplt${id}`}
+                    className="absolute inset-y-0 -start-2 flex items-center font-sans text-xl font-black text-gray-200 transition hover:text-gray-400"
+                  >
+                    {id}
+                  </a>
+                )}
+                {originalText?.map((verse) => (
+                  <div className="flex justify-center">
+                    <Verse content={verse} />
+                  </div>
+                ))}
+              </div>
+
+              {(urTrans || enTrans) && (
+                <div className="col-span-3 mt-4 flex-col justify-center gap-y-0.5 text-center lg:mt-0 lg:flex">
+                  {urTrans && (
+                    <div className="text-center font-nastaliq text-xl leading-[2]" dir="rtl">
+                      {urduText}
+                    </div>
+                  )}
+                  {enTrans && (
+                    <div className="text-center font-nastaliq text-sm" dir="ltr">
+                      {englishText}
+                    </div>
+                  )}
+                </div>
+              )}
+            </div>
+          </SizeProvider>
+        );
+      })}
+    </>
+  );
+}
+
+function Verse({ content }: { content: string }) {
+  const verseRef = useRef<HTMLDivElement>(null);
+  const sizeContext = use(SizeContext);
+
+  useEffect(() => {
+    if (verseRef.current?.clientWidth && sizeContext?.setChildSize) {
+      sizeContext.setChildSize(verseRef.current?.clientWidth);
+    }
+  }, []);
+
+  return (
+    <div
+      ref={verseRef}
+      style={{ minWidth: sizeContext?.maxSize ? `${sizeContext?.maxSize}px` : undefined }}
+      className={clsx("inline-block w-fit leading-[1.8]", sizeContext?.maxSize && "flex justify-between")}
+    >
+      {content
+        .split(" ")
+        .filter(Boolean)
+        .map((word) => (
+          <div className="inline-block px-0.5">{word}&nbsp;</div>
+        ))}
+    </div>
+  );
+}
+
+export function SizeProvider({ children }: PropsWithChildren<{}>) {
+  const [maxSize, setMaxSize] = useState<number>();
+
+  return (
+    <SizeContext.Provider
+      value={{
+        maxSize,
+        setChildSize: (size) => {
+          setMaxSize((s) => Math.max(s ?? 0, size));
+        },
+      }}
+    >
+      {children}
+    </SizeContext.Provider>
+  );
+}
--- src/app/[book]/[section]/[poem]/page.tsx
+++ src/app/[book]/[section]/[poem]/page.tsx
@@ -1,7 +1,10 @@
+import { SizeProvider, Stanza } from "./components";
+import type PoemType from '@/assets/content/armaghan-e-hijaz/01/01.json';
+
 export default async function Poem({ params }: { params: Promise<{ book: string; section: string; poem: string }> }) {
   const { book, section, poem: poemId } = await params;
 
-  const poem = await import(`../../../../assets/content/${book}/${section}/${poemId}.json`).then((m) => m.default);
+  const poem = await import(`../../../../assets/content/${book}/${section}/${poemId}.json`).then((m) => m.default as typeof PoemType);
 
   const urTrans = true;
   const enTrans = true;
@@ -13,11 +16,11 @@ export default async function Poem({ params }: { params: Promise<{ book: string;
           <div>Translation:</div>
         </div>
         <div className="flex items-center gap-x-2">
-          <input className="relative -top-0.5" checked={enTrans} type="checkbox" id="show-en-translation" />
+          <input className="relative -top-0.5" defaultChecked={enTrans} type="checkbox" id="show-en-translation" />
           <label htmlFor="show-en-translation">English</label>
         </div>
         <div className="flex items-center gap-x-2">
-          <input className="relative -top-0.5" checked={urTrans} type="checkbox" id="show-ur-translation" />
+          <input className="relative -top-0.5" defaultChecked={urTrans} type="checkbox" id="show-ur-translation" />
           <label htmlFor="show-ur-translation">Urdu</label>
         </div>
       </div>
